f6878ead609f00cb0a6445ad96153f1b4b1e8d71
neeraja991 <59947818+neeraja991@users.noreply.github.com>
2020-11-13 23:53:00 -0500

    Create parsev1.js

---
+++ parsev1.js
@@ -0,0 +1,53 @@
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/', function (req, res, next) {
+    next();
+});
+app.get('/api/v1/parse', function (req, res, next) {
+    res.send('<h2>V1 Parse</h2><br><form action="/post1" method="POST"> <input type="text" name="value1"> <button type="submit"> Send </button> </form>');
+});
+app.post('/post1', function (req, res, next) {
+    console.log('data from v1: ', req.body.value1);
+    res.status(200).send('OK')
+    res.redirect('/api/v1/parse');
+});
+
+var server = http.createServer(app);
+server.listen(3000);
+
+app.use('/', function (req, res, next) {
+    res.send('<h1> first midleware </h1>');
+    // res.status(200).send('OK')
+
+function sayName(o) {
+    var str = "JOHN0000MICHAEL0009994567";
+    var fN = str.substring(0, 8);
+    var fN1 = fN.split("0");
+    var fN2 = fN1.splice(0, 1);
+    console.log(fN2);
+    var lN = str.substring(8, 18);
+    var lN1 = lN.split("0");
+    var lN2 = lN1.splice(0, 1);
+    console.log(lN2);
+    var cI = str.substring(18, 25);
+    console.log(cI);
+    //convert string to Json and display in console
+    const obj = {
+        firstName: fN2,
+        lastName: lN2,
+        clientId: cI
+    };
+    const data = JSON.stringify(obj);
+    console.log(data);
+    
+    // {"firstName":["JOHN"],"lastName":["MICHAEL"],"clientId":"9994567"}
+}
+const v2p = {
+    name: "JOHN0000MICHAEL0009994567"
+};
+sayName(v2p);
+});
+

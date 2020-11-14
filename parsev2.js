3e951a8b0ff611c1dc42ef1795b7f3773d5fc6f5
neeraja991 <59947818+neeraja991@users.noreply.github.com>
2020-11-14 02:04:16 -0500

    Update parsev2.js

--- parsev2.js
+++ parsev2.js
@@ -1,29 +1,21 @@
 "use strict";
-const http = require('http');
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/', (req, res, next) => {
+app.use('/', function (req, res, next) {
     next();
 });
-app.get('/api/v2/parse', (req, res, next) => {
-    res.send('<h2>V2 Parse</h2><br><form action="/post2" method="POST"> <input type="text" name="value2"> <button type="submit"> Send </button> </form>');
+app.get('/api/v2/parse', function (req, res, next) {
+    res.send('<h2>V1 Parse</h2><br><form action="/post2" method="POST"> <input type="text" name="value2"> <button type="submit"> Send </button> </form>');
 });
-app.post('/post2', (req, res, next) => {
+app.post('/post2', function (req, res, next) {
     console.log('data from v2: ', req.body.value2);
-    res.status(200).send('OK');
-    res.end();
-    res.redirect('/api/v2/parse');
-});
-app.use('/', (req, res, next) => {
-    res.send('<h1> second middleware</h1>');
-});
-const server = http.createServer(app);
-server.listen(3000);
-
-function sayName(o) {
-    var str = "JOHN0000MICHAEL0009994567";
+       
+        function sayName(o) {
+    var str = o.body.value2;
+    console.log(str);
     var fN = str.substring(0, 8);
     var fN1 = fN.split("0");
     var fN2 = fN1.splice(0, 1);
@@ -45,13 +37,25 @@ function sayName(o) {
     };
     const data = JSON.stringify(obj);
     console.log(data);
-    // {"firstName":["JOHN"],"lastName":["MICHAEL"],"clientId":"999-4567"}
+    
+   // res.status(200).send('OK');
+    res.end(JSON.stringify({data}));
+
+    // {"firstName":["JOHN"],"lastName":["MICHAEL"],"clientId":"9994567"}
 }
-const v1p = {
+const v2p = {
     name: "JOHN0000MICHAEL0009994567"
 };
-sayName(v1p);
+sayName(req);
 
+    
+});
 
+var server = http.createServer(app);
+server.listen(3000);
 
-
+app.use('/post2', function (req, res, next) {
+    //res.send('<h1> first midleware </h1>');
+     res.status(200).send('OK');
+    
+});
